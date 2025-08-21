8b89f46ecdc0c355c7cb7a7722bf8df7b85d5199
Murel02 <olimurel@gmail.com>
2025-08-21 23:54:37 +0200

    Udvid landing picker med flere funktioner og ny styling

--- app.js
+++ app.js
@@ -9,24 +9,24 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // Liste med mulige landing spots
- const spots = [
-  { name: 'The Hive', x: 43, y: 39 },
-  { name: 'Swarmy Stash', x: 61, y: 39 },
-  { name: 'Ranger\'s Ruin', x: 29, y: 69 },
-  { name: 'O.X.R. HQ', x: 63, y: 44 },
-  { name: 'Supernova Academy', x: 60, y: 13 },
-  { name: 'First Order Base', x: 83, y: 20 },
-  { name: 'Shining Span', x: 83, y: 56 },
-  { name: 'Utopia City', x: 72, y: 56 },
-  { name: 'Foxy Floodgate', x: 48, y: 66 },
-  { name: 'Shiny Shafts', x: 26, y: 47 },
-  { name: 'Outlaw Oasis', x: 16, y: 50 },
-  { name: 'Resistance Base', x: 13, y: 19 },
-  { name: 'Canyon Crossing', x: 42, y: 85 },
-  { name: 'Outpost Enclave', x: 89, y: 85 },
-  { name: 'Kappa Kappa Factory', x: 81, y: 65 },
-  { name: 'Shogun\'s Solitude', x: 20, y: 91 },
-  { name: 'Demon\'s Debris', x: 56, y: 81 }
+const spots = [
+  { name: 'The Hive',            x: 40, y: 45 },
+  { name: 'Swarmy Stash',        x: 58, y: 42 },
+  { name: "Ranger's Ruin",       x: 25, y: 65 },
+  { name: 'O.X.R. HQ',           x: 60, y: 48 },
+  { name: 'Supernova Academy',   x: 55, y: 15 },
+  { name: 'First Order Base',    x: 85, y: 20 },
+  { name: 'Shining Span',        x: 85, y: 55 },
+  { name: 'Utopia City',         x: 70, y: 62 },
+  { name: 'Foxy Floodgate',      x: 50, y: 70 },
+  { name: 'Shiny Shafts',        x: 30, y: 48 },
+  { name: 'Outlaw Oasis',        x: 15, y: 50 },
+  { name: 'Resistance Base',     x: 10, y: 20 },
+  { name: 'Canyon Crossing',     x: 35, y: 78 },
+  { name: 'Outpost Enclave',     x: 88, y: 85 },
+  { name: 'Kappa Kappa Factory', x: 75, y: 67 },
+  { name: "Shogun's Solitude",   x: 20, y: 90 },
+  { name: "Demon's Debris",      x: 55, y: 80 }
 ];
 
 app.get('/', (req, res) => {
